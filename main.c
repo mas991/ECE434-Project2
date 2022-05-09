02daa0abf062101c1bd5a96ed6b2b6573a9bdcd3
mas991 <94929415+mas991@users.noreply.github.com>
2022-05-09 10:51:17 -0400

    Update main.c

--- main.c
+++ main.c
@@ -20,9 +20,17 @@ void sig_func(int signum) {
 int main() {
     
     pthread_t tid[NUMBER_OF_TEAMS][THREADS_PER_TEAM];
-    signal(SIGINT,sig_func);
-    signal(SIGABRT,sig_func);
-    signal(SIGILL,sig_func);
+    
+    void (*oldHandlers[8])(int);
+    
+    oldHandlers[0] = signal(SIGINT,sig_func);
+    oldHandlers[1] = signal(SIGABRT,sig_func);
+    oldHandlers[2] = signal(SIGILL,sig_func);
+    oldHandlers[3] = signal(SIGCHLD,SIG_IGN);
+    oldHandlers[4] = signal(SIGSEGV,SIG_IGN);
+    oldHandlers[5] = signal(SIGFPE,SIG_IGN);
+    oldHandlers[6] = signal(SIGHUP,SIG_IGN);
+    oldHandlers[7] = signal(SIGTSTP,SIG_IGN);
     
     for(int i=0;i<NUMBER_OF_TEAMS;i++) {
         for(int j=0;j<THREADS_PER_TEAM;j++) {
@@ -37,6 +45,11 @@ int main() {
             pthread_kill(tid[i][j],SIGINT);
             pthread_kill(tid[i][j],SIGABRT);
             pthread_kill(tid[i][j],SIGILL);
+            pthread_kill(tid[i][j],SIGCHLD);
+            pthread_kill(tid[i][j],SIGSEGV);
+            pthread_kill(tid[i][j],SIGFPE);
+            pthread_kill(tid[i][j],SIGHUP);
+            pthread_kill(tid[i][j],SIGTSTP);
         }
     }
     
@@ -47,4 +60,15 @@ int main() {
             pthread_join(tid[i][j],NULL);
         }
     }
+    
+    signal(SIGINT,oldHandlers[0]);
+    signal(SIGABRT,oldHandlers[1]);
+    signal(SIGILL,oldHandlers[2]);
+    signal(SIGCHLD,oldHandlers[3]);
+    signal(SIGSEGV,oldHandlers[4]);
+    signal(SIGFPE,oldHandlers[5]);
+    signal(SIGHUP,oldHandlers[6]);
+    signal(SIGTSTP,oldHandlers[7]);
+    
+    exit(1);
 }
